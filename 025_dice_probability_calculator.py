2a3263a12abc97d865019ef12644fc0463053d4e
manwperlel <manwperlel@gmail.com>
2026-08-21 21:13:26 -0300

    new

--- 025_dice_probability_calculator.py
+++ 025_dice_probability_calculator.py
@@ -1,6 +1,7 @@
 limit = int(input("Enter a limit: "))
 counter = 0
 total = 0
+
 side1 = 0
 side2 = 0
 side3 = 0
@@ -12,13 +13,12 @@ while counter < limit:
     counter = counter + 1
     number = int(input(f"Roll number {counter}: "))
 
-    # Validate if the number is outside the die range (1 to 6)
+
     if number < 1 or number > 6:
         print("Invalid number. Must be between 1 and 6.")
-        counter = counter - 1  # Retry this turn
-        print(counter)
+        counter = counter - 1 
     else:
-        total = total + number  # Only add to total if valid
+        total = total + number
         
         if number == 1:
             side1 = side1 + 1
@@ -33,13 +33,14 @@ while counter < limit:
         elif number == 6:
             side6 = side6 + 1
 
-# Percentage calculations
+
 average_side1 = (side1 / limit) * 100
 average_side2 = (side2 / limit) * 100
 average_side3 = (side3 / limit) * 100
 average_side4 = (side4 / limit) * 100
 average_side5 = (side5 / limit) * 100
 average_side6 = (side6 / limit) * 100
+
 
 print(f"\nSide 1: {average_side1}%")
 print(f"Side 2: {average_side2}%")
